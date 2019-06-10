64a1cbf26f09d06992d5165cdc4cb155579251fb
J. Dirani <juliendirani@gmail.com>
2019-06-10 15:51:42 +0400

    Update preproc.py

--- preproc.py
+++ preproc.py
@@ -15,6 +15,7 @@
 import mne, eelbrain, os, glob, pickle
 import numpy as np
 import pandas as pd
+from os.path import join
 %gui qt
 mne.set_log_level(verbose='WARNING')
 
@@ -22,7 +23,7 @@ mne.set_log_level(verbose='WARNING')
 expt = 'TwoTones' #experiment name as written on the -raw.fif
 ROOT = '/Users/my_user/data_file/'
 subjects_dir = os.path.join(ROOT, 'MRI')
-subjects = ['A0176', 'A0192', 'A0216']
+subjects = [i for i in os.listdir(ROOT+'MEG') if i.startswith('A0')]
 event_id = dict(hi=223, lo=191) #conditions associated with each trigger
 os.chdir(ROOT) #setting current dir
 
@@ -30,6 +31,8 @@ epoch_tmin = -0.1
 epoch_tmax = 0.6
 epoch_baseline = (-0.1,0)
 decim = 1 # 1 for no decimation
+equalize_epochs = False
+expected_nb_events = 136
 
 photodiode_realignment = True
 SNR = 3 # 3 for ANOVAs, 2 for regressions
@@ -100,6 +103,8 @@ for subj in subjects:
         #-----------------------------Find events------------------------------#
         print "%s: Finding events..." %subj
         events = mne.find_events(raw,min_duration=0.002)
+        if len(events) != expected_nb_events:
+            print "WARNING: unexpected nb of events found (not %s)"%expected_nb_events
         picks_meg = mne.pick_types(raw.info, meg=True, eeg=False, eog=False, stim=False)
 
         #-----------------------Fix photodiode shift---------------------------#
@@ -115,7 +120,7 @@ for subj in subjects:
             print 'Realigning events...'
             for i in range(len(events)):
                 events[i][0] = photodiode[i][0]
-        
+
         #----------------------Reject epochs based on logs---------------------#
         # print "Rejecting epochs based on logs"
         # logfile_dir = 'MEG/%s/initial_output/%s_log_mask.csv' %(subj,subj)
@@ -152,15 +157,42 @@ for subj in subjects:
             rejfile = eelbrain.load.unpickle('MEG/%s/%s_rejfile.pickled' %(subj, subj))
             rejs = rejfile['accept'].x
             epochs_rej = epochs[rejs]
-        print '%s: epochs_rej created' %subj
+
+        # equalize events
+        if equalize_epochs == True:
+            print "equalizing epochs..."
+            len_epochs_rej = len(epochs_rej) #because epochs_rej will be overwritten. Need this to later log to info.csv
+            epochs_eq, eq_ids = epochs_rej.equalize_event_counts(event_id) #ids used later to log into info.csv
+        else:
+            print "not equalizing epochs."
+            epochs_eq = epochs_rej.copy()
+            eq_ids = []
+            len_epochs_rej = len(epochs_rej)
+
+        print '%s: bad epochs rejected' %subj
+
 
 
         #------------------Save raw.info and epochs to file-------------------#
         print 'Saving epochs to file...'
         info = raw.info
         pickle.dump(info, open('MEG/%s/%s-info.pickled' %(subj,subj), 'wb'))
-        epochs_rej.save('MEG/%s/%s-epo.fif' %(subj,subj))
-        
+        epochs_eq.save('MEG/%s/%s-epo.fif' %(subj,subj))
+
+        # write rejection info to csv
+        nb_accuracy_rej = np.unique(accuracy_mask, return_counts=True)[1][0]
+        nb_noise_rej = np.unique(rejs, return_counts=True)[1][0]
+        with open('MEG/%s/info/rej_info.csv'%subj, 'w') as f:
+            f.write('accuracy_rej,noise_rej,good,equalize_counts,equalize_dropped,remaining_after_equalize\n')
+            f.write('%s,%s,%s,%s,%s,%s\n'%(nb_accuracy_rej, nb_noise_rej, len_epochs_rej, equalize_epochs, len(eq_ids),len(epochs_eq)))
+            f.close()
+
+        # Write epochs counts per condition to csv
+        with open('MEG/%s/info/epo_count.csv'%subj, 'w') as f:
+            for cond in event_id.keys():
+                f.write('%s,%s\n'%(cond, len(epochs_eq[cond])))
+            f.close()
+
         del raw
         print 'Done.'
 
@@ -178,7 +210,12 @@ for subj in subjects:
         info = pickle.load(open('MEG/%s/%s-info.pickled' %(subj,subj), 'rb'))
         epochs_rej = mne.read_epochs('MEG/%s/%s-epo.fif' %(subj,subj))
         trans = mne.read_trans('MEG/%s/%s-trans.fif' %(subj,subj))
-        bem = glob.glob('MRI/%s/bem/*-bem-sol.fif' %subj)[0]
+
+        bem_fname = join(subjects_dir, subj, 'bem/%s-bem-sol.fif'%subj)
+        src_fname = join(subjects_dir, '%s/bem/%s-ico-4-src.fif' %(subj,subj))
+        fwd_fname = 'MEG/%s/%s-fwd.fif' %(subj, subj)
+        cov_fname = 'MEG/%s/%s-cov.fif' %(subj,subj)
+
 
 
     #---------------------------get evoked------------------------------------#
@@ -189,43 +226,50 @@ for subj in subjects:
             evoked.append(epochs_rej[cond].average())
         print 'Done.'
 
+        # Sanity check: plot evoked
+        if not os.path.isdir('sanity_check/evoked/'):
+            os.makedirs('sanity_check/evoked/')
+        all_evokeds = mne.combine_evoked(evoked, weights='equal')
+        evoked_plot = all_evokeds.plot_joint(show=False)
+        evoked_plot.savefig('sanity_check/evoked/%s_evoked.png'%subj)
+
 
         #----------------------Source space---------------------------#
         print 'Generating source space...'
-        if os.path.isfile('MRI/%s/bem/%s-ico-4-src.fif' %(subj,subj)):
+        if os.path.isfile(src_fname):
             print 'src for subj = %s already exists, loading file...' %subj
-            src = mne.read_source_spaces(fname='MRI/%s/bem/%s-ico-4-src.fif' %(subj,subj))
+            src = mne.read_source_spaces(fname=src_fname)
             print 'Done.'
         else:
             print 'src for subj = %s does not exist, creating file...' %subj
             src = mne.setup_source_space(subject=subj, spacing='ico4', subjects_dir=subjects_dir)
-            src.save('MRI/%s/bem/%s-ico-4-src.fif' %(subj,subj), overwrite=True)
+            src.save(src_fname, overwrite=True)
             print 'Done. File saved.'
 
 
         #--------------------Forward solution-------------------------#
         print 'Creating forward solution...'
-        if os.path.isfile('MEG/%s/%s-fwd.fif' %(subj, subj)):
+        if os.path.isfile(fwd_fname):
             print 'forward soltion for subj=%s exists, loading file.' %subj
-            fwd = mne.read_forward_solution('MEG/%s/%s-fwd.fif' %(subj, subj))
+            fwd = mne.read_forward_solution(fwd_fname)
             print 'Done.'
         else:
             print 'forward soltion for subj=%s does not exist, creating file.' %subj
-            fwd = mne.make_forward_solution(info=info, trans=trans, src=src, bem=bem, ignore_ref=True)
-            mne.write_forward_solution('MEG/%s/%s-fwd.fif' %(subj, subj), fwd)
+            fwd = mne.make_forward_solution(info=info, trans=trans, src=src, bem=bem_fname, ignore_ref=True)
+            mne.write_forward_solution(fwd_fname, fwd)
             print 'Done. File saved.'
 
 
         #----------------------Covariance------------------------------#
         print 'Getting covariance'
-        if os.path.isfile('MEG/%s/%s-cov.fif' %(subj,subj)):
+        if os.path.isfile(cov_fname):
             print 'cov for subj=%s exists, loading file...' %subj
-            cov=mne.read_cov('MEG/%s/%s-cov.fif' %(subj,subj))
+            cov = mne.read_cov(cov_fname)
             print 'Done.'
         else:
             print 'cov for subj=%s does not exist, creating file...' %subj
             cov = mne.compute_covariance(epochs_rej,tmin=None,tmax=0, method=['shrunk', 'diagonal_fixed', 'empirical'])
-            cov.save('MEG/%s/%s-cov.fif' %(subj,subj))
+            cov.save(cov_fname)
             print 'Done. File saved.'
 
 
@@ -243,9 +287,8 @@ for subj in subjects:
         os.makedirs('STC/%s' %subj)
         for ev in evoked:
             stc = mne.minimum_norm.apply_inverse(ev, inv, lambda2=lambda2, method='dSPM')
-            #mophing stcs to the fsaverage using precomputed matrix method:
+            # mophing stcs to the fsaverage using precomputed matrix method:
             vertices_to = mne.grade_to_vertices('fsaverage', grade=4, subjects_dir=subjects_dir) #fsaverage's source space
-#             stc_morph = mne.morph_data(subject_from=subj, subject_to='fsaverage',stc_from=stc, grade=vertices_to,subjects_dir=subjects_dir)
             morph_mat = mne.compute_morph_matrix(subject_from=subj, subject_to='fsaverage', vertices_from=stc.vertices, vertices_to=vertices_to, subjects_dir=subjects_dir)
             stc_morph = mne.morph_data_precomputed(subject_from=subj, subject_to='fsaverage', stc_from=stc, vertices_to=vertices_to, morph_mat=morph_mat)
             stc_morph.save('STC/%s/%s_%s_dSPM' %(subj,subj,ev.comment))
@@ -254,4 +297,4 @@ for subj in subjects:
         print '-----------------------------------------\n'
 
         #deleting variables
-        del epochs_rej, evoked, info, trans, bem, src, fwd, cov, inv
+        del epochs_rej, evoked, info, trans, src, fwd, cov, inv
